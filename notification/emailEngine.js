257ddced0b2c782e69a8ae856f9fd75b3533dfcd
davix-tech <davixsir654@gmail.com>
2026-06-17 09:18:29 +0100

    Update emailEngine.js

--- notification/emailEngine.js
+++ notification/emailEngine.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.re_dpn1smF1_Jgfz9M7eNZAWGkvnsnHSvES7);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 /* ─────────────────────────────────────────────
    CORE SEND ENGINE
